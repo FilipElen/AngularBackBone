e1e05353033674a9bbb72f0fe6912a7eca3fdfac
FilipElen <filipelengit@outlook.com>
2017-10-11 17:52:18 +0200

    personsSrv Werkt

--- app/app.js
+++ app/app.js
@@ -7,16 +7,47 @@ angular.module('App', ['ngRoute'])
     .when('/home', {
       templateUrl: 'assets/views/home.html',
       controller: 'homeCtrl'
+    }).when('/persons',{
+      templateUrl: 'assets/views/persons.html',
+      controller: 'personsCtrl'
     })
     .otherwise({
       redirectTo: '/home'
     });
 })
-
+.controller('homeCtrl', ['$scope', function homeCtrl($scope) {}])
+.controller('personsCtrl',['$scope', 'personSrv', function($scope, personSrv){
+  $scope.persons = personSrv.getAllPersons();
+  console.log($scope.persons);
+}])
+.service('personSrv',function(){
+  var persons = [{
+    'id': 0,
+    'name' : 'Winston Churchill'
+  },{
+    'id': 1,
+    'name' : 'Albert Einstein'
+  }];
+  return {
+    getAllPersons: function(){
+      return persons;
+    },
+    getPerson: function(personId){
+      for (var i = 0; i < persons.length; i++) {
+        if (persons[i] == personId) {
+          return perons[i];
+        }
+        return null;
+      }
+    }
+  }
+})
+.service('quotersSrv', function(){
+  var quotes[
+    id : 0,
+    quotes:['quote Churchill1', 'quote Churchill2']
+  ];
+})
 .config(function($compileProvider) {
   $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|sms):/);
 })
-
-.controller('homeCtrl', ['$scope', function homeCtrl($scope) {
-  
-}])
